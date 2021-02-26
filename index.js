031fc73c2f10cf8dd1a588f078415dee086b2654
Nicholas V Frangione <nfrangione8@gmail.com>
2021-02-26 10:02:41 -0800

    commit tree 6f4ad0847f4b96c20f31e76c0d56a2a2a522451c

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let greetingForFeet = '';
+  if (feet <= 400) {
+    greetingForFeet = 'This one is on me!';
+  }
+  else if (feet > 2000 && feet <= 2500) {
+    greetingForFeet = 'I will gladly take your thirty bucks.';
+  }
+  else if (feet > 2500) {
+    greetingForFeet = 'No can do.'
+  }
+  return greetingForFeet;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const isNYC = (city === 'NYC') ? true:false
+  let cityCheck = ''
+  if (isNYC === true) {
+    cityCheck = 'Ok, sounds good.'
+  }
+  else {
+    cityCheck = 'No go.'
+  }
+  return cityCheck;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let message = '';
+  switch (tip) {
+    case 'generous':
+      message = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      message = 'Thank you.'
+      break;
+    default: 
+      message = 'Bye.'
+      break;
+  }
+  return message;
 }
